c8b15e8c8bec4a8134d45ef636e10a3953bc2380
lee <leeeorzol@gmail.com>
2019-12-31 14:17:49 -0500

    initial commit

---
+++ THMServer/Controllers/HighscoreController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace THMServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HighscoreController : ControllerBase
+    {
+        private const string FILENAME = "../highscores.json";
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<HighscoreController> _logger;
+
+        public HighscoreController(ILogger<HighscoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Highscore> Get()
+        {
+            if (System.IO.File.Exists(FILENAME))
+            {
+                var file = System.IO.File.ReadAllText(FILENAME);
+                var highscores = JsonConvert.DeserializeObject<IEnumerable<Highscore>>(file);
+                return highscores;
+            }
+            return new List<Highscore>();
+        }
+
+        [HttpPost]
+        public IEnumerable<Highscore> PostHighscore(Highscore highscore)
+        {
+            var highscores = Get()?.ToList();
+            if(highscores == null)
+            {
+                highscores = new List<Highscore>();
+            }
+
+            highscores.Add(highscore);
+
+            if (!System.IO.File.Exists(FILENAME))
+            {
+                System.IO.File.Create(FILENAME);
+            }
+            System.IO.File.WriteAllText(FILENAME, JsonConvert.SerializeObject(highscores.ToList()));
+
+            return highscores;
+        }
+    }
+}
---
+++ THMServer/Highscore.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace THMServer
+{
+    public class Highscore
+    {
+        public string Date { get; set; }
+
+        public int Score { get; set; }
+
+        public string User { get; set; }
+    }
+}
